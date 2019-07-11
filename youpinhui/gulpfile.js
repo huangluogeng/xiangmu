346d875f9661e99017e225a4fc48a2b727b0529a
huangluogeng <2545786146@qq.com>
2019-07-11 20:13:49 +0800

    7月11日第一次提交

--- youpinhui/gulpfile.js
+++ youpinhui/gulpfile.js
@@ -1,8 +1,9 @@
-const gulp = requir('gulp');
+const gulp = require('gulp');
 const watch = require('gulp-watch'); //引入监听的gulp插件
 const minihtml = require('gulp-minify-html'); //引入html的压缩插件
 const comfilesass = require('gulp-sass'); //编译sass
 const minicss = require('gulp-minify-css');
+
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
@@ -13,13 +14,13 @@ const es2015 = require('babel-preset-es2015');
 gulp.task('uglifyhtml', () => {
     return gulp.src('src/*.html') //引入文件
         .pipe(minihtml()) //执行压缩插件
-        .pipe(gulp.dest('dist/')); //输出
+        .pipe(gulp.dest('dist/')); //输出 
 });
 
 
 //编译压缩sass-->css
-gulp.task('compilesass', () => {
-    return gulp.src('src/sass/*.scss')
+gulp.task('minicss', () => {
+    return gulp.src('src/*.css')
         .pipe(comfilesass({ //执行sass编译
             outputStyle: 'compressed'
         }))
@@ -29,8 +30,8 @@ gulp.task('compilesass', () => {
 
 //压缩js
 gulp.task('uglifyjs', () => {
-    return gulp.src('src/js/*.js') //引入文件
-        .pipe(uglify()) //执行压缩插件
+    return gulp.src('src/script/*.js') //引入文件1111111
+        //执行压缩插件
         .pipe(gulp.dest('dist/js')); //输出
 })
 
@@ -38,7 +39,8 @@ gulp.task('uglifyjs', () => {
 //压缩图片
 
 gulp.task('uglifyimage', () => {
-    return gulp.src('src/images/*/*.{png,jpg,gif,ico}')
+
+    return gulp.src('src/img/*/*.{png,jpg,gif,ico}')
         .pipe(imagemin({
             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
             progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
@@ -59,12 +61,14 @@ gulp.task('uglifypng', () => {
         .pipe(gulp.dest('dist/images'));
 });
 
-//es6转es5
+// //es6转es5
 //gulp-babel gulp-core  babel-preset-es2015
 gulp.task('babeljs', () => {
     return gulp.src('src/js/*.js') //引入文件
         .pipe(babel({
             presets: ['es2015']
         })) //执行压缩插件
+        .pipe(uglify())
         .pipe(gulp.dest('dist/js')); //输出
+
 });
--- youpinhui/src/script/js/details.js
+++ youpinhui/src/script/js/details.js
@@ -1,181 +1,39 @@
-;
-! function() {
-
-    function setcookie(key, value, day) {
-        let date = new Date();
-        date.setDate(date.getDate() + day);
-        document.cookie = key + '=' + encodeURI(value) + ';expires=' + date;
+$(".img").hover(function() {
+    $(".bimg").css("display", "block");
+    $("#move").css("display", "block");
+}, function() {
+    $(".bimg").css("display", "none");
+    $("#move").css("display", "none");
+});
+//需要为小图片的div添加一个鼠标移动时间
+$(".img").mousemove(function(event) {
+    //需要获取鼠标移动是距离左边和顶端的距离
+    var x = event.pageX;
+    var y = event.pageY;
+    //需要计算放大区域的需要移动到的位置
+    var nx = x - $(".img").offset().left - $("#move").width() / 2;
+    var ny = y - $(".img").offset().top - $("#move").height() / 2;
+    //判断移动后是否已经超出的范围
+    if (nx < 0) { //左边超出
+        nx = 0;
     }
-
-    function getcookie(key) {
-        let arr = decodeURI(document.cookie).split('; ');
-        for (let i = 0; i < arr.length; i++) {
-            let newarr = arr[i].split('=');
-            if (newarr[0] === key) {
-                return newarr[1];
-            }
-        }
+    if (nx > $(".img").width() - $("#move").width()) { //右边超出
+        nx = $(".img").width() - $("#move").width();
     }
-
-    function delcookie(key) {
-        setcookie(key, '', -1);
+    if (ny < 0) { //顶端超出
+        ny = 0;
     }
-    //1.获取商品列表页传来的id
-    let sid = location.search.substring(1).split('=')[1];
-    const spic = document.querySelector('.spic');
-    const bpic = document.querySelector('.bpic');
-    const sf = document.querySelector('.sf');
-    const bf = document.querySelector('.bf');
-    const title = document.querySelector('.title');
-    const price = document.querySelector('.price');
-    const movelistUl = document.querySelector('.movelist ul');
-    const details = document.querySelector('.details');
-    const left = document.querySelector('#left');
-    const right = document.querySelector('#right');
-    const btn = document.querySelector('.goodsnum button');
-    const goodsnumcount = document.querySelector('.goodsnum input');
-    const linkbox = document.querySelector('.linkbox');
-    const close = document.querySelector('.linkbox span');
-    const closeA = document.querySelectorAll('.linkbox a');
-    close.onclick = closeA[1].onclick = function() {
-        linkbox.style.display = 'none';
+    if (ny > $(".img").height() - $("#move").height()) { //底部超出
+        ny = $(".img").height() - $("#move").height();
     }
-    ajax({
-        url: 'http://localhost/xiangmu/youpinhui/php/',
-        data: {
-            picid: sid
-        },
-        dataType: 'json',
-        success: function(objdata) {
-            //1.拼接放大镜图片等信息
-            spic.querySelector('img').src = objdata.url;
-            spic.querySelector('img').setAttribute('sid', objdata.picid);
-            bpic.src = objdata.url;
-            title.innerHTML = objdata.title;
-            price.innerHTML = objdata.price;
-            //拼接小图列表。
-            let ullist = objdata.imgurls.split(',');
-            let lihtml = '';
-            for (let i = 0; i < ullist.length; i++) {
-                lihtml += '<li><img src="' + ullist[i] + '"></li>';
-            }
-            movelistUl.innerHTML = lihtml;
-            console.log(ullist);
-        }
+    //设置放大区域的移动
+    $("#move").css({
+        left: nx + "px",
+        top: ny + "px"
     });
-    //实现放大镜效果
-    spic.onmouseover = function() {
-        sf.style.visibility = 'visible';
-        bf.style.visibility = 'visible';
-        //求宽高。
-        sf.style.width = spic.offsetWidth * bf.offsetWidth / bpic.offsetWidth + 'px';
-        sf.style.height = spic.offsetHeight * bf.offsetHeight / bpic.offsetHeight + 'px';
-
-        //求比例
-        let bili = bpic.offsetWidth / spic.offsetWidth;
-
-        this.onmousemove = function(ev) {
-            var ev = ev || window.event;
-            let l = ev.clientX - details.offsetLeft - sf.offsetWidth / 2;
-            let t = ev.clientY - details.offsetTop - sf.offsetHeight / 2;
-            if (l < 0) {
-                l = 0;
-            } else if (l >= spic.offsetWidth - sf.offsetWidth - 2) {
-                l = spic.offsetWidth - sf.offsetWidth - 2;
-            }
-
-            if (t < 0) {
-                t = 0;
-            } else if (t >= spic.offsetHeight - sf.offsetHeight - 2) {
-                t = spic.offsetHeight - sf.offsetHeight - 2;
-            }
-
-
-            sf.style.left = l + 'px';
-            sf.style.top = t + 'px';
-
-            bpic.style.left = -l * bili + 'px';
-            bpic.style.top = -t * bili + 'px';
-        }
-    };
-    spic.onmouseout = function() {
-        sf.style.visibility = 'hidden';
-        bf.style.visibility = 'hidden';
-    };
-    //事件委托给li添加事件
-    movelistUl.onmouseover = function(ev) {
-            var ev = ev || window.event;
-            let ele = ev.target || ev.srcElement;
-
-            if (ele.parentNode.nodeName == 'LI') {
-                let imgurl = ele.parentNode.querySelector('img').src;
-                spic.querySelector('img').src = imgurl;
-                bpic.src = imgurl;
-            }
-        }
-        //给ullist左右箭头添加点击事件。
-    let num = 5; //默认显示小图的张数。
-    right.onclick = function() {
-        let icolist = movelistUl.querySelectorAll('li'); //li的长度
-        let liwidth = icolist[0].offsetWidth + 5; //1个li的宽度
-        if (icolist.length > num) { //如果当前li列表的长度>5,后面还有图片。
-            num++;
-            left.style.color = '#333';
-
-            if (num == icolist.length) {
-                right.style.color = '#fff';
-            }
-
-            buffermove(movelistUl, { left: -(num - 5) * liwidth });
-        }
-
-    };
-    left.onclick = function() {
-        let icolist = movelistUl.querySelectorAll('li'); //li的长度
-        let liwidth = icolist[0].offsetWidth + 5; //1个li的宽度
-        if (num > 5) {
-            num--;
-            right.style.color = '#333';
-            if (num == 5) {
-                left.style.color = '#fff';
-            }
-            buffermove(movelistUl, { left: -(num - 5) * liwidth });
-        }
-
-    }
-
-    //购物车的操作
-    let arrsid = []; //商品的编号sid [1,3,5]
-    let arrnum = []; //商品的数量 [12,24,36]
-
-    //如果cookie存在，获取cookie的值，并转换成数组。
-    function cookievalue() {
-        if (getcookie('cookiesid') && getcookie('cookienum')) {
-            arrsid = getcookie('cookiesid').split(','); //将获取的cookie转换成数组
-            arrnum = getcookie('cookienum').split(',');
-        }
-    }
-
-
-
-
-    btn.onclick = function() {
-        //确定按钮是第一次还是多次。
-        //先获取cookie的值,而且是一个数组。
-        cookievalue();
-        //当前按钮对应得sid，如果当前按钮对应得sid存在arrsid中，存在
-        let sid = spic.querySelector('img').getAttribute('sid'); //当前页面的sid
-        if (arrsid.indexOf(sid) === -1) { //不存在
-            arrsid.push(sid);
-            arrnum.push(goodsnumcount.value);
-            setcookie('cookiesid', arrsid.toString(), 10);
-            setcookie('cookienum', arrnum.toString(), 10);
-        } else { //存在，数量累计
-            let sum = Number(arrnum[arrsid.indexOf(sid)]) + Number(goodsnumcount.value); //获取累加的值
-            arrnum[arrsid.indexOf(sid)] = sum;
-            setcookie('cookienum', arrnum.toString(), 10);
-        }
-
-        linkbox.style.display = 'block';
-    };
-}();
+    //设置大图片的移动   大图片的移动方向与放大区域正好相反
+    $(".bimg>img").css({
+        left: -nx * $("#move").width() / $(".simg").width() + "px",
+        top: -ny * $("#move").height() / $(".simg").height() + "px"
+    });
+});
